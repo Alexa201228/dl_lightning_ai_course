1bda53d2ac25241e42c2618d908c845af46a3b8b
Alexa201228 <aleksandra.grid2012@gmail.com>
2023-10-19 21:15:09 +0300

    add validation with lightning

--- optimize_with_lightning/lightning_mlp.py
+++ optimize_with_lightning/lightning_mlp.py
@@ -4,8 +4,9 @@ import lightning as L
 import torch
 import torch.nn.functional as F
 from lightning import Trainer
+import torchmetrics
 
-from tools import PyTorchMLP, compute_accuracy, get_dataset_loaders
+from tools import PyTorchMLP, get_dataset_loaders
 
 
 class LightningModel(L.LightningModule):
@@ -16,23 +17,37 @@ class LightningModel(L.LightningModule):
 
         self._learning_rate = learning_rate
         self._model = model
+        self._train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=10)
+        self._val_acc = torchmetrics.Accuracy(task="multiclass", num_classes=10)
+        self._test_acc = torchmetrics.Accuracy(task="multiclass", num_classes=10)
 
+    def _shared_step(self, batch):
+        features, true_labels = batch
+        logits = self(features)
+        loss = F.cross_entropy(logits, true_labels)
+
+        predicted_labels = torch.argmax(logits, dim=1)
+
+        return loss, true_labels, predicted_labels
     def forward(self, x) -> Any:
         return self._model(x)
 
     def training_step(self, batch, batch_idx):
 
-        features, true_labels = batch
-        logits = self(features)
-        loss = F.cross_entropy(logits, true_labels)
-        self.log("train_loss", loss)
+        loss, true_labels, predicted_labels = self._shared_step(batch)
+        self._train_acc(predicted_labels, true_labels)
+        self.log("train_acc", self._train_acc, prog_bar=True, on_epoch=True, on_step=False)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        features, true_labels = batch
-        logits = self(features)
-        loss = F.cross_entropy(logits, true_labels)
-        self.log("val_loss", loss, prog_bar=True)
+        loss, true_labels, predicted_labels = self._shared_step(batch)
+        self._val_acc(predicted_labels, true_labels)
+        self.log("val_acc", self._val_acc, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        loss, true_labels, predicted_labels = self._shared_step(batch)
+        self._test_acc(predicted_labels, true_labels)
+        self.log("test_acc", self._test_acc, prog_bar=False)
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self._learning_rate)
@@ -60,11 +75,12 @@ if __name__ == "__main__":
         val_dataloaders=val_loader,
     )
 
-    train_acc = compute_accuracy(pytorch_model, train_loader, device)
-    val_acc = compute_accuracy(pytorch_model, val_loader, device)
-    test_acc = compute_accuracy(pytorch_model, test_loader, device)
+    train_acc = trainer.test(dataloaders=train_loader)[0]["test_acc"]
+    val_acc = trainer.test(dataloaders=val_loader)[0]["test_acc"]
+    test_acc = trainer.test(dataloaders=test_loader)[0]["test_acc"]
 
-    print(f"Train acc: {train_acc * 100:.2f}%")
-    print(f"Val acc: {val_acc * 100:.2f}%")
-    print(f"Test acc: {test_acc * 100:.2f}%")
+    print(f"Train acc {train_acc * 100:.2f}%")
+    print(f"Val acc {val_acc * 100:.2f}%")
+    print(f"Test acc {test_acc * 100:.2f}%")
+
 
